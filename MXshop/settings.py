fcbab14b55a0cb8a62170a07efc64ae69e7af97e
270225324@qq.com <270225324@qq.com>
2020-03-26 11:19:28 +0800

    更改了数据库密码为虚拟密码

--- MXshop/settings.py
+++ MXshop/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MxShop',
         'USER':'root',
-        'PASSWORD':'qwert4',
+        'PASSWORD':'qwer1234567',
         'HOST':'127.0.0.1',
         'OPTIONS':{'init_command':'SET default_storage_engine=INNODB;'}
     }
